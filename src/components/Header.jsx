98079d44b6101b7316b0685e2b5217cc9075db08
xrishabhjx <xrishabhjx@gmail.com>
2025-04-06 12:21:05 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = ({ title }) => {
     return (
-        <header className='sticky top-0 bg-green-800 bg-opacity-50 backdrop-blur-md shadow-lg border-b border-green-700 z-20'>
+        <header className='sticky top-0 bg-green-800 bg-opacity-50 backdrop-blur-md shadow-lg border-b border-green-700 z-20 flex justify-center items-center p-8'>
             <div className='py-4 px-6'>
                 <h1 className='text-2xl font-semibold text-gray-100'>{title}</h1>
             </div>
